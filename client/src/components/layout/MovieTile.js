0c74b076ea056f44415a1e6cca220ce0d0150808
Solomon Montagno <sjmontagno@gmail.com>
2023-05-03 09:59:45 -0400

    Minor changes to styling

--- client/src/components/layout/MovieTile.js
+++ client/src/components/layout/MovieTile.js
@@ -5,14 +5,14 @@ const MovieTile = ( { movie }) => {
     return ( 
         <div className="grid-x grid-margin-x callout border">
         <div className="cell small-3">
-          <img className="movie-poster" src={movie.movieImageUrl} alt="movie-posters"></img>
+            <img className="movie-poster" src={movie.movieImageUrl} alt="movie-posters"></img>
         </div>
         <div className="cell small-9 border">
-          <h4><strong>{movie.title}</strong></h4>
-          <p>Release Year: {movie.year}</p>
-          <p>Genre: {movie.genre}</p>
+            <h4><strong>{movie.title}</strong></h4>
+            <p>Release Year: {movie.year}</p>
+            <p>Genre: {movie.genre}</p>
         </div>
-      </div>
+    </div>
     )
 }
 
