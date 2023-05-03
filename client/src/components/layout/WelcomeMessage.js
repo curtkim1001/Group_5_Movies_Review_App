ac5ad131f8b36a9da799a2002e4b3f6e6fcf894b
Solomon Montagno <sjmontagno@gmail.com>
2023-05-03 16:06:53 -0400

    fix indentation for welcome

--- client/src/components/layout/WelcomeMessage.js
+++ client/src/components/layout/WelcomeMessage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import three from "../../../public/images/space.png"
 
-const WelcomeMessage = (props) =>{
+const WelcomeMessage = (props) => {
     return (
         <div>
             <h2 className="welcome-message">Reel Reviews</h2>
             <h4 className="welcome-message">Take a seat, enjoy the space.</h4>
-            <img className="logo" src={three}/>
+            <img className="logo" src={three} />
             <p className="authors">Developed and Designed by: Curt, Jose, and Solomon</p>
-            </div>
+        </div>
     )
 }
 
