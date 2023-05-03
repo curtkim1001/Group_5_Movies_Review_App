a00dc911a049fff0ed7936ae9e463f95152686f5
Curt Kim <dhk2431@gmail.com>
2023-05-03 16:11:21 -0400

    changes per nicks comments

--- client/src/components/layout/MovieTile.js
+++ client/src/components/layout/MovieTile.js
@@ -3,20 +3,19 @@ import { Redirect, Link } from "react-router-dom"
 
 const MovieTile = ( { movie }) => {
 
-
     return ( 
         <>
             <Link to={`/movies/${movie.id}`}>
-            <div className="grid-x grid-margin-x callout border">
-            <div className="cell small-3">
-                <img className="movie-poster" src={movie.movieImageUrl} alt="movie-posters"></img>
-            </div>
-            <div className="small-6 border">
-                <h4><strong>{movie.title}</strong></h4>
-                <p>Release Year: {movie.year}</p>
-                <p>Genre: {movie.genre}</p>
-            </div>
-            </div>
+                <div className="grid-x grid-margin-x callout border">
+                    <div className="cell small-3">
+                        <img className="movie-poster" src={movie.movieImageUrl} alt="movie-posters"></img>
+                    </div>
+                    <div className="small-6 border">
+                        <h4>{movie.title}</h4>
+                        <p>Release Year: {movie.year}</p>
+                        <p>Genre: {movie.genre}</p>
+                    </div>
+                </div>
             </Link>
         </>
         )
