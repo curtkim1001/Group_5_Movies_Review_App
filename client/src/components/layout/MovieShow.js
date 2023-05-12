6891736b16d681acf13c8852714c677909d3a9b8
Solomon Montagno <sjmontagno@gmail.com>
2023-05-12 12:25:32 -0400

    Delete edits

--- client/src/components/layout/MovieShow.js
+++ client/src/components/layout/MovieShow.js
@@ -4,33 +4,33 @@ import MovieReviewForm from "./MovieReviewForm.js";
 import { useParams } from "react-router-dom";
 
 const MovieShow = (props) => {
-  let visibleReviewFormComponent;
-  const { id } = useParams();
+    let visibleReviewFormComponent;
+    const { id } = useParams();
 
-  const [movie, setMovie] = useState({
-    title: "",
-    year: "",
-    genre: "",
-    synopsis: "",
-    movieImageUrl: "",
-  });
-  const [reviews, setReviews] = useState([]);
+    const [movie, setMovie] = useState({
+        title: "",
+        year: "",
+        genre: "",
+        synopsis: "",
+        movieImageUrl: "",
+    });
+    const [reviews, setReviews] = useState([]);
 
-  const getMovie = async () => {
-    try {
-      const response = await fetch(`/api/v1/movies/${id}`);
-      if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`;
-        const error = new Error(errorMessage);
-        throw error;
-      }
-      const movieData = await response.json();
-      setMovie(movieData.movie);
-      setReviews(movieData.movie.reviews);
-    } catch (error) {
-      console.error(`Error in fetch: ${error.message}`);
-    }
-  };
+    const getMovie = async () => {
+        try {
+            const response = await fetch(`/api/v1/movies/${id}`);
+            if (!response.ok) {
+                const errorMessage = `${response.status} (${response.statusText})`;
+                const error = new Error(errorMessage);
+                throw error;
+            }
+            const movieData = await response.json();
+            setMovie(movieData.movie);
+            setReviews(movieData.movie.reviews);
+        } catch (error) {
+            console.error(`Error in fetch: ${error.message}`);
+        }
+    };
 
     const getAverageRating = () => {
         let total = 0
@@ -57,22 +57,22 @@ const MovieShow = (props) => {
         visibleReviewFormComponent = null
     }
 
-  return (
-    <div className="movie-show grid-container grid-x">
-      <h2>{movie.title}</h2>
-      <div className="cell small-10 medium-8 large-8 content">
-        <div className="callout movie-content rounded-corner">
-          <p>Release Year: {movie.year}</p>
-          <p>Genre: {movie.genre}</p>
-          <p>Description: {movie.synopsis}</p>
+    return (
+        <div className="movie-show grid-container grid-x">
+            <h2>{movie.title}</h2>
+            <div className="cell small-10 medium-8 large-8 content">
+                <div className="callout movie-content rounded-corner">
+                    <p>Release Year: {movie.year}</p>
+                    <p>Genre: {movie.genre}</p>
+                    <p>Description: {movie.synopsis}</p>
                     <p>Average Rating: {getAverageRating()}</p>
+                </div>
+                <img src={movie.movieImageUrl} alt="movie-poster"></img>
+            </div>
+            <ReviewList movieReviews={reviews} movieId={id} user={props.user} />
+            <div className="review-form">{visibleReviewFormComponent}</div>
         </div>
-        <img src={movie.movieImageUrl} alt="movie-poster"></img>
-      </div>
-      <ReviewList movieReviews={reviews} user={props.user}  />
-      <div className="review-form">{visibleReviewFormComponent}</div>
-    </div>
-  );
+    );
 };
 
 export default MovieShow;
--- client/src/components/layout/ReviewTile.js
+++ client/src/components/layout/ReviewTile.js
@@ -36,15 +36,14 @@ const ReviewTile = props => {
             console.error(`Error in fetch: ${error.message}`)
         }
     }
-  };
 
-  const handleUpVote = (reviewId) => {
-    handleVote(1, reviewId);
-  };
+    const handleUpVote = (reviewId) => {
+        handleVote(1, reviewId);
+    };
 
-  const handleDownVote = (reviewId) => {
-    handleVote(-1, reviewId);
-  };
+    const handleDownVote = (reviewId) => {
+        handleVote(-1, reviewId);
+    };
 
     let message = ""
     if (props.review.spoilerWarning === true) {
@@ -84,26 +83,26 @@ const ReviewTile = props => {
         }
     }
 
-  return (
-    <div className="callout review-show rounded-corner">
-      <p>Username is: {props.review.user.username}</p>
-      <div className="spoiler-text">
-        <p>{message}</p>
-      </div>
-      <h4>{props.review.content}</h4>
-      <p>Rating: {props.review.rating}</p>
-      <p>Total votes: {voteTotal}</p>
-      <p onClick={() => handleUpVote(props.review.id)}>
-        <FontAwesomeIcon icon={faThumbsUp} />
-      </p>
-      <p onClick={() => handleDownVote(props.review.id)}>
-        <FontAwesomeIcon icon={faThumbsDown} />
-      </p>
+    return (
+        <div className="callout review-show rounded-corner">
+            <p>Username is: {props.review.user.username}</p>
+            <div className="spoiler-text">
+                <p>{message}</p>
+            </div>
+            <h4>{props.review.content}</h4>
+            <p>Rating: {props.review.rating}</p>
+            <p>Total votes: {voteTotal}</p>
+            <p onClick={() => handleUpVote(props.review.id)}>
+                <FontAwesomeIcon icon={faThumbsUp} />
+            </p>
+            <p onClick={() => handleDownVote(props.review.id)}>
+                <FontAwesomeIcon icon={faThumbsDown} />
+            </p>
             <div>
                 {hideDelete}
             </div>
-    </div>
-  );
-};
+        </div>
+    );
+}
 
 export default ReviewTile;
